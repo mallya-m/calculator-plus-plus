6e5668323e65dc255f478e18dfa770edf63116da
Mallya <mallyamoni035@gmail.com>
2026-01-14 04:05:24 +0530

    feat: evaluate tokenized expression with operator precedence

--- app.js
+++ app.js
@@ -26,3 +26,29 @@ function tokenize(expr){
 
     return tokens;
 }
+
+function evaluateTokens(tokens){
+    let result = [...tokens];
+
+    for(let i = 0 ; i < result.length ; i++){
+        if(result[i] === "*" || result[i] === "/"){
+            const left = result[i-1];
+            const right = result[i+1];
+            const value = result[i]=== "*" ? left * right : left / right ;
+
+            result.splice(i-1, 3 , value);
+            i--;
+        }
+    }
+    for(let i=0; i< result.length ; i++){
+        if(result[i] === "+" || result[i] === "-"){
+            const left = result[i-1];
+            const right = result[i+1];
+            const value = result[i]==="+" ? left+right : left - right ;
+
+            result.splice(i-1,3,value);
+            i-- ;
+        }
+        return result[0];
+    }
+}
