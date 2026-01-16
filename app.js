ff59683ef741d1762728d602cc3eeb72891f8546
Mallya <mallyamoni035@gmail.com>
2026-01-16 23:04:52 +0530

    feat : keyboard support handling

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const display = document.getElementById("expression");
+const display = document.getElementById("currentNumber");
 const buttons = document.querySelectorAll(".btn");
 
-let expression = "";
+let currentNumber = "";
 
 //Tokenizer
 function tokenize(expr){
     const tokens = [];
-    let currentNumber = "";
+    let tempNumber = "";
 
     for(let char of expr){
-        if(!isNaN(char) || char === "."){
-            currentNumber += char;
+        if( (char >= "0" && char <= "9")  || char === "."){
+            tempNumber += char;
         }else{
-            if(currentNumber!== ""){
-                tokens.push(Number(currentNumber));
-                currentNumber="";
+            if(tempNumber!== ""){
+                tokens.push(Number(tempNumber));
+                tempNumber="";
             }
             tokens.push(char);
         }
     }
-    if (currentNumber !== "") {
-    tokens.push(Number(currentNumber));
+    if (tempNumber !== "") {
+    tokens.push(Number(tempNumber));
     }
 
     return tokens;
 }
-
+//Evaluation
 function evaluateTokens(tokens){
     let result = [...tokens];
 
@@ -48,10 +48,10 @@ function evaluateTokens(tokens){
             result.splice(i-1,3,value);
             i-- ;
         }
-        return result[0];
     }
+    return result[0];
 }
-
+//Button click handling
 buttons.forEach(button =>{
     button.addEventListener("click",()=>{
         const value = button.dataset.value;
@@ -60,20 +60,17 @@ buttons.forEach(button =>{
 });
 
 function handleInput(value){
-    if(value === "C"){
-        clearAll();
-    }else if(value === "="){
-        calculate();
-    }else {
-        appendValue(value);
-    }
+    if(value === "C") clearAll();
+    else if(value === "=") calculate();
+    else appendValue(value);
 }
 function updateDisplay(){
     display.textContent = currentNumber || 0 ;
 }
+
 function appendValue(value){
     if(currentNumber === "0"){
-        currentNumber = "";
+        currentNumber = value;
     }else{
         currentNumber += value ;
     }
@@ -91,6 +88,35 @@ function calculate(){
         updateDisplay();
     }catch{
         currentNumber = "";
-        updateDisplay();
+        display.textContent = "error";
     }
 }
+//Keyboard support
+
+document.addEventListener("keydown",(e)=>{
+    const key = e.key ;
+    if (
+        (key >= "0" && key <="9")||
+        key === "+" || key ==="-"||
+        key === "*" || key ==="/"||
+        key === "."
+    ){
+        handleInput(key);
+    }
+    if(key=== "Enter"){
+        e.preventDefault();
+        handleInput("=");
+    }
+    if(key === "Backspace"){
+        deleteLast();
+    }
+    if(key === "Escape"){
+        handleInput("C");
+    }
+});
+
+function deleteLast(){
+    currentNumber = currentNumber.slice(0,-1);
+    updateDisplay();
+}
+
